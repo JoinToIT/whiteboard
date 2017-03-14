50c5ae3ce609710e9d9e332d612063dbecce4bd1
Igor Zhovtaniuk <igorzhovtyj@gmail.com>
2017-03-14 10:32:55 +0200

    First commit;[[C

---
+++ config.php
@@ -0,0 +1,18 @@
+<?php
+
+$host = 'http://' . $_SERVER['HTTP_HOST'];
+define('_MAIN_TITLE_', 'White board');
+define('_FIRST_TITLE_', 'Welcome'); 
+define('__HOST__', $host);
+define('_MAIN_DOC_ROOT_', __DIR__);
+define( 'DB_HOST', 'localhost' );
+define( 'DB_USER', 'developer_test8' );
+define( 'DB_PASS', '2rn5x3HgVa' );
+define( 'DB_NAME', 'developer_test8' );
+define( 'SEND_ERRORS_TO', '' );
+define('UPLOAD_DIR', dirname(__FILE__).'/downloads');
+//define( 'DISPLAY_DEBUG', false );
+//define( 'SITENAME', '' );
+define( 'ADMINEMAIL', 'sergiy.tonkoshkuryk@jointoit.com' );
+//date_default_timezone_set('Australia/Sydney');
+
